d680ef62f630bb4cd94a7884d1fa28bedab5a9c6
abhayhegde <abhayhegde.skp@gmail.com>
2021-06-20 01:58:14 +0530

    changed paragraph margins

--- @narative/gatsby-theme-novela/src/components/Paragraph/Paragraph.tsx
+++ @narative/gatsby-theme-novela/src/components/Paragraph/Paragraph.tsx
@@ -7,7 +7,7 @@ const Paragraph = styled.p`
   color: ${(p) => p.theme.colors.articleText};
   font-family: ${(p) => p.theme.fonts.sansSerif};
   transition: ${(p) => p.theme.colorModeTransition};
-  margin: 0 auto 50px;
+  margin: 0 auto 35px;
   width: 100%;
   max-width: 980px;
 
